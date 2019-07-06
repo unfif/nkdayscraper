88c6f8200823c3ca142b2afdc5b3d584d54f957e
unfif <undo5@hotmail.co.jp>
2019-07-06 14:12:18 +0900

    progress_02

--- nkc01/dbmeta.py
+++ nkc01/dbmeta.py
@@ -1,14 +1,20 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, select, and_, or_, not_
+import pandas as pd
 
-engine = create_engine("postgresql+psycopg2://postgres:marehito@localhost:5432/postgres")
-
-print(engine.table_names())
-meta = MetaData(engine)
+engine = create_engine("postgresql+psycopg2://postgres:marehito@localhost:5432/postgres", echo=False)
+# print(engine.table_names())
+# meta = MetaData(engine)
+meta = MetaData()
 meta.reflect(bind=engine)
+# print(meta.tables.keys())
+table = meta.tables['nkthedayraces']
+cols = table.c
+# print(cols.keys())
+con = engine.connect()
+sql = table.select().where(cols.raceid.like('2019030202__')).where(cols.placenum.in_([1, 2, 3])).order_by(cols.racenum).order_by(cols.placenum)
+# sql = "SELECT * FROM nkthedayraces AS nk "
+# sql += "WHERE nk.raceid LIKE '2019030202__' AND nk.placenum IN (1, 2, 3) "
+# sql += "ORDER BY nk.racenum, nk.placenum"
+pd.read_sql_query(sql, con)
 
-print(meta.tables.keys())
-# con = engine.connect()
-# res = con.execute("SELECT * FROM NKTHEDAYRACES")
-# for row in res: print(row)
-
-# con.close()
+con.close()
--- nkcflask01/model.py
+++ nkcflask01/model.py
@@ -1,6 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
+import pandas as pd
 
 db = SQLAlchemy()
+engine = db.create_engine("postgresql+psycopg2://postgres:marehito@localhost:5432/postgres", echo=False)
+meta = db.MetaData()
+meta.reflect(bind=engine)
+table = meta.tables['nkthedayraces']
+cols = table.c
 
 def init_db(app):
   app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:marehito@localhost:5432/postgres"#?charset=utf8"
@@ -12,6 +18,14 @@ class ToDoItem(db.Model):
   item_id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(100), nullable=False, default=False)
   done = db.Column(db.Boolean, nullable=False)
+
+class NkTheDayRaces():
+    def getRaces():
+        con = engine.connect()
+        sql = table.select().where(cols.raceid.like('2019030202__')).where(cols.placenum.in_([1, 2, 3])).order_by(cols.racenum).order_by(cols.placenum)
+        racesdf = pd.read_sql_query(sql, con)
+        con.close()
+        return racesdf
 
 class ToDoList:
   def add(self, title):
