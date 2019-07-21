76bfec8047963ca3edb318f0f297f3a51a22942d
unfif <undo5@hotmail.co.jp>
2019-07-22 02:48:18 +0900

    progress_2019072201

--- jsondf01.py
+++ jsondf01.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 import pathlib as pl
 df = pd.read_json('C:/Users/pathz/Documents/scrapy/nkc01/results01.json', encoding='utf-8')
-df[['place', 'jockey', 'placenum']].query('placenum < 4').groupby(['place', 'jockey', 'placenum']).count()
-df[['place', 'racenum', 'jockey', 'placenum']].query('placenum < 4').sort_values(['place', 'racenum', 'placenum']).count()
+df[['place', 'jockey', 'placenum']].query('placenum < 4').groupby(['place', 'jockey', 'placenum'])#.count(level='placenum')
+df[['place', 'jockey', 'placenum']].query('placenum < 4').sort_values(['place', 'jockey', 'placenum']).set_index(['place', 'jockey']).apply(pd.value_counts)#.placenum.value_counts()#.nunique()#.count(level='jockey')
 df.columns
 df.groupby(['place', 'racenum']).jockey.groups
-df.groupby(['place', 'jockey', 'placenum']).query('placenum < 4').count()
+df.groupby(['place', 'jockey', 'placenum']).query('placenum < 4')
 jockeygp = df.query('placenum < 4').groupby(['place', 'jockey']).placenum
 jockeygp.count()
 jockeygp[jockeygp['placenum'] < 4]
