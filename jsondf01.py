4b6880ebe19bd937ad749d9d7413496d418a369f
unfif <undo5@hotmail.co.jp>
2019-07-23 20:10:26 +0900

    progress_2019072302

--- jsondf01.py
+++ jsondf01.py
@@ -2,19 +2,23 @@ import numpy as np
 import pandas as pd
 # import pathlib as pl
 # pl.Path.cwd()
-df = pd.read_json('C:/Users/pathz/Documents/scrapy/nkc01/results01.json', encoding='utf-8')
-# dfp = df[['place', 'jockey', 'placenum']]#.query('placenum < 4')
-# dfp = df[['place', 'jockey', 'placenum'] and df['placenum'] < 4]
-jockeyct = pd.crosstab([df.place, df.jockey], df.placenum, margins=True)
-jockeyct['単勝率'] = round(100 * jockeyct[1] / jockeyct.All, 2)
-jockeyct['連対率'] = round(100 * jockeyct[2] / jockeyct.All, 2)
-jockeyct['複勝率'] = round(100 * jockeyct[3] / jockeyct.All, 2)
-jockeyct.loc[:, [1,2,3,'単勝率', '連対率', '複勝率', 'All']].sort_values(['place',1,2,3], ascending=False)
+jockeyct = pd.read_json('C:/Users/pathz/Documents/scrapy/nkc01/results01.json', encoding='utf-8')
+# dfp = jockeyct[['place', 'jockey', 'placenum']]#.query('placenum < 4')
+# dfp = jockeyct[['place', 'jockey', 'placenum'] and jockeyct['placenum'] < 4]
+data = {}
+jockeyct = pd.crosstab([jockeyct.place, jockeyct.jockey], jockeyct.placenum, margins=True)
+rates = [round(100 * jockeyct[rank] / jockeyct.All, 2) for rank in range(1, 4)]
+jockeyct['単勝率'], jockeyct['連対率'], jockeyct['複勝率'] = rates
+data['jockeys'] = jockeyct.loc[:, [1,2,3,'単勝率', '連対率', '複勝率', 'All']].sort_values(['place',1,2,3], ascending=False)
+data['jockeys'] = data['jockeys'].rename(columns={1:'1着',2:'2着',3:'3着','All':'騎乗数'})
+for i, d in data['jockeys'].iterrows(): print(d)
 
-df.query('placenum < 4').groupby(['place', 'jockey', 'placenum'])['placenum'].count().unstack().fillna(0)
-df[['place', 'jockey', 'placenum']].query('placenum < 4').groupby(['place', 'jockey', 'placenum'])#.count(level='placenum')
-df[['place', 'jockey', 'placenum']].query('placenum < 4').sort_values(['place', 'jockey', 'placenum']).set_index(['place', 'jockey']).apply(pd.value_counts)#.placenum.value_counts()#.nunique()#.count(level='jockey')
-df.columns
-df.groupby(['place', 'racenum']).jockey.groups
-jockeygp = df.query('placenum < 4').groupby(['place', 'jockey']).placenum
+
+
+jockeyct.query('placenum < 4').groupby(['place', 'jockey', 'placenum'])['placenum'].count().unstack().fillna(0)
+jockeyct[['place', 'jockey', 'placenum']].query('placenum < 4').groupby(['place', 'jockey', 'placenum'])#.count(level='placenum')
+jockeyct[['place', 'jockey', 'placenum']].query('placenum < 4').sort_values(['place', 'jockey', 'placenum']).set_index(['place', 'jockey']).apply(pd.value_counts)#.placenum.value_counts()#.nunique()#.count(level='jockey')
+jockeyct.columns
+jockeyct.groupby(['place', 'racenum']).jockey.groups
+jockeygp = jockeyct.query('placenum < 4').groupby(['place', 'jockey']).placenum
 jockeygp.count()
