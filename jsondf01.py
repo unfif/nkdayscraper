747270894f04758f73075a3f8d0a5dad5e766609
unfif <undo5@hotmail.co.jp>
2019-07-25 04:33:26 +0900

    progress_2019072501

--- jsondf01.py
+++ jsondf01.py
@@ -2,16 +2,25 @@ import numpy as np
 import pandas as pd
 # import pathlib as pl
 # pl.Path.cwd()
-jockeyct = pd.read_json('C:/Users/pathz/Documents/scrapy/nkc01/results01.json', encoding='utf-8')
+racesdf = pd.read_json('C:/Users/pathz/Documents/scrapy/nkc01/results01.json', encoding='utf-8')
 # dfp = jockeyct[['place', 'jockey', 'placenum']]#.query('placenum < 4')
 # dfp = jockeyct[['place', 'jockey', 'placenum'] and jockeyct['placenum'] < 4]
 data = {}
-jockeyct = pd.crosstab([jockeyct.place, jockeyct.jockey], jockeyct.placenum, margins=True)
+jockeyct = pd.crosstab([racesdf.place, racesdf.jockey], racesdf.placenum, margins=True)
 rates = [round(100 * jockeyct[rank] / jockeyct.All, 2) for rank in range(1, 4)]
 jockeyct['単勝率'], jockeyct['連対率'], jockeyct['複勝率'] = rates
-data['jockeys'] = jockeyct.loc[:, [1,2,3,'単勝率', '連対率', '複勝率', 'All']].sort_values(['place',1,2,3], ascending=False)
-data['jockeys'] = data['jockeys'].rename(columns={1:'1着',2:'2着',3:'3着','All':'騎乗数'})
-for i, d in data['jockeys'].iterrows(): print(d)
+jockeyct = jockeyct.loc[:, [1,2,3, '単勝率', '連対率', '複勝率', 'All']].sort_values(['place',1,2,3], ascending=False)
+data['jockeys'] = jockeyct.rename(columns={1:'1着',2:'2着',3:'3着','All':'騎乗数'})
+for th in data['jockeys'].columns:
+    print(th)
+
+data['jockeys']['1着']
+
+for index, row in data['jockeys'].iterrows():
+    print(row)
+
+d.name
+i[0]
 
 
 
