b17e2b91008582d3cd932f36eed9f6e8c0b6c91a
unfif <undo5@hotmail.co.jp>
2019-09-08 14:21:43 +0900

    #3 add dropping a table in init

--- nkdayscraper/pipelines.py
+++ nkdayscraper/pipelines.py
@@ -8,18 +8,17 @@
 from sqlalchemy.orm import sessionmaker
 from nkdayscraper.models import Nkdayraces, db_connect, create_table
 
-class NkdayscraperPipeline(object):
+class NkdayscraperPipeline():
     def __init__(self):
         """Initializes database connection and sessionmaker. Creates deals table."""
         engine = db_connect(False)
-        # drop_table(engine, Nkdayraces)
+        if engine.has_table('nkdayraces'): Nkdayraces.__table__.drop(engine)
         create_table(engine)
         self.Session = sessionmaker(bind=engine)
         session = self.Session()
-        session.query(Nkdayraces).delete()
+        # session.query(Nkdayraces).delete()
         session.commit()
         model = Nkdayraces()
-        # print(model.getCommentList())
 
     # def open_spider(self, spider: scrapy.Spider):# コネクションの開始
     #     DATABASE_URL = nkdayscraper.settings.get('DATABASE_URL')
@@ -36,6 +35,7 @@ class NkdayscraperPipeline(object):
         try:
             session.add(nkdbraces)
             session.commit()
+            pass
         except:
             session.rollback()
             raise
