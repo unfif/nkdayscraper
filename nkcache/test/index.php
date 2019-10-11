8b9813ef7329e7799a339208225832ea1a280b34
unfif <undo5@hotmail.co.jp>
2019-10-11 23:52:27 +0900

    fix #18 add-function-commented-out-for-debug-pages

---
+++ nkcache/test/index.php
@@ -0,0 +1,81 @@
+<?php header("Content-Type: text/html;charset=EUC-JP") ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="EUC-JP">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <div class="RaceList_Box fc">
+    <dl class="race_top_hold_list">
+      <dt class="race_top_hold_data">
+        <p class="kaisaidata">4回中山9日目<span class="paybacklink"><a href="/?pid=payback_list&id=p2019060409"><img src="style/netkeiba.ja/image/btn_racelist_payback.png" /></a></span></p>
+        <p class="jyodata">
+          天気:曇&nbsp;/&nbsp;芝:良&nbsp;&nbsp;ダ:良
+        </p>
+      </dt>
+      <dd>
+        <ul>
+          <li>
+            <dl class="race_top_data_info fc">
+              <dt><a href="/pid=race&id=p201906040901&mode=top.php" title="２歳未勝利"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/race_num_list_img_01_close.png" alt="1R" /></a></dt>
+              <dd>
+                <div class="racename">
+                  <a href="/pid=race&id=p201906040901&mode=top.php" title="２歳未勝利">２歳未勝利</a><b id="news_201906040901"></b>
+                </div>
+                <div class="racedata">
+                  <a href="https://db.netkeiba.com/?pid=movie&id=201906040901" title="レース映像" target="_blank"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/icon_movie_01.gif" alt="レース映像" class="movieicon" /></a>
+
+
+                  10:05&nbsp;ダ1800m&nbsp;16頭<br />
+                </div>
+              </dd>
+            </dl>
+          </li>
+          <li>
+            <dl class="race_top_data_info fc">
+              <dt><a href="/pid=race&id=p201906040902&mode=top.php" title="２歳未勝利"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/race_num_list_img_02_close.png" alt="2R" /></a></dt>
+              <dd>
+                <div class="racename">
+                  <a href="/pid=race&id=p201906040902&mode=top.php" title="２歳未勝利">２歳未勝利</a><b id="news_201906040902"></b>
+                </div>
+                <div class="racedata">
+                  <a href="https://db.netkeiba.com/?pid=movie&id=201906040902" title="レース映像" target="_blank"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/icon_movie_01.gif" alt="レース映像" class="movieicon" /></a>
+
+
+                  10:35&nbsp;芝1200m&nbsp;16頭<br />
+                </div>
+              </dd>
+            </dl>
+          </li>
+          <li>
+            <dl class="race_top_data_info fc">
+              <dt><a href="/pid=race&id=p201906040903&mode=top.php" title="２歳未勝利"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/race_num_list_img_03_close.png" alt="3R" /></a></dt>
+              <dd>
+                <div class="racename">
+                  <a href="/pid=race&id=p201906040903&mode=top.php" title="２歳未勝利">２歳未勝利</a><b id="news_201906040903"></b>
+                </div>
+                <div class="racedata">
+                  <a href="https://db.netkeiba.com/?pid=movie&id=201906040903" title="レース映像" target="_blank"><img src="https://cdn.netkeiba.com/img.race/style/netkeiba.ja/image/icon_movie_01.gif" alt="レース映像" class="movieicon" /></a>
+
+
+                  11:05&nbsp;芝1600m&nbsp;16頭<br />
+                </div>
+              </dd>
+            </dl>
+          </li>
+        </ul>
+      </dd>
+    </dl>
+  </div><!-- /.race_list -->
+  <div class="InfoTxt_Box">
+    <p>
+      ※結果・成績・オッズなどのデータは、必ず主催者発表のものと照合しご確認ください。
+    </p>
+  </div>
+
+  <!-- -->
+</body>
+</html>
--- nkdayscraper/pipelines.py
+++ nkdayscraper/pipelines.py
@@ -8,29 +8,32 @@
 from sqlalchemy.orm import sessionmaker
 from nkdayscraper.models import Nkdayraces, db_connect, create_table, mongo_connect
 import datetime as dt
+import copy as cp
 
 class NkdayscraperPipeline():
+    cnt = 0;
     def __init__(self):
         """Initializes database connection and sessionmaker. Creates deals table."""
-        self.engine = db_connect()
+        self.engine = db_connect(echo=False)
         if self.engine.has_table('nkdayraces'): Nkdayraces.__table__.drop(self.engine)
         create_table(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
         self.mongoerr = False
-        mongo = mongo_connect(query={'serverSelectionTimeoutMS': 3000})
-        self.mongodb = mongo.netkeiba
+        self.mongo = mongo_connect(query={'serverSelectionTimeoutMS': 3000})
+        self.mongodb = self.mongo.netkeiba
         self.collection = self.mongodb.nkdayraces
 
         try: self.collection.drop()
         except: self.mongoerr = True
 
-    # def open_spider(self, spider: scrapy.Spider):# コネクションの開始
-    #     DATABASE_URL = nkdayscraper.settings.get('DATABASE_URL')
-    #     self.conn = psycopg2.connect(DATABASE_URL)
-    #
-    # def close_spider(self, spider: scrapy.Spider):# コネクションの終了
-    #     self.conn.close()
+    def open_spider(self, spider):
+        # DATABASE_URL = nkdayscraper.settings.get('DATABASE_URL')
+        # self.conn = psycopg2.connect(DATABASE_URL)
+        pass
+
+    def close_spider(self, spider):
+        self.mongo.close()
 
     def process_item(self, item, spider):
         """Save deals in the database. This method is called for every item pipeline component."""
@@ -38,9 +41,7 @@ class NkdayscraperPipeline():
         if self.engine.name not in ['postgresql', 'mongodb']:
             item['addedmoneylist'] = str(item['addedmoneylist'])
             item['positionlist'] = str(item['positionlist'])
-
         nkdbraces = Nkdayraces(**item)
-
         try:
             session.add(nkdbraces)
             session.commit()
@@ -51,13 +52,9 @@ class NkdayscraperPipeline():
             session.close()
 
         if not self.mongoerr:
-            posttimeorig = item['posttime']
-            timeorig = item['time']
-            item['posttime'] = str(item['posttime'])
-            item['time'] = str(item['time'])
-            self.collection.insert_one(dict(item))
-            item['posttime'] = posttimeorig
-            item['time'] = timeorig
-
+            mongoitem = cp.deepcopy(item)
+            mongoitem['posttime'] = str(mongoitem['posttime'])
+            mongoitem['time'] = str(mongoitem['time'])
+            self.collection.insert_one(dict(mongoitem))
 
         return item
