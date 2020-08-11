7cf78b15ba215a13e614be98190a1156e9bb03d2
unfif <age@sage.com>
2020-08-11 14:16:31 +0900

    Bootstrap 5 alphaを導入し、bootstrap-vueの使用を廃止する修正 fix #58, fix #53

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 // import VueCompositionAPI from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import 'popper.js/dist/popper.min.js'
+// import 'bootstrap/dist/js/bootstrap.min.js'
+import 'popper.js'
+import 'bootstrap'
 
 createApp(App).mount('#app')
