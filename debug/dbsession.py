5d792dfbc4aa636f01196e2d1f0acd9bc01e3fc7
unfif <age@sage.com>
2021-05-07 23:00:32 +0900

    DBのModelを拡張できるように整理して一般化 fix #139

--- debug/dbsession.py
+++ debug/dbsession.py
@@ -1,9 +1,9 @@
 from sqlalchemy.orm import sessionmaker
-from ..models import horseresults, db_connect#, create_tables
+from ..models import horseresults, db_connect#, Base
 
 engine = db_connect(echo=True)
 # drop_table(engine, horseresults)
-# create_tables(engine)
+# Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine, future=False)
 session = Session()
 records = session.query(horseresults).all()
