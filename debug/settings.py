575418ae9c94fae088081f4699a456c61b4df85b
unfif <age@sage.com>
2021-07-12 18:54:58 +0900

    HerokuのDATABASE_URLをSQLAlchemy 1.4でも利用できるように修正_2021071201 fix #187

--- debug/settings.py
+++ debug/settings.py
@@ -7,6 +7,7 @@ load_dotenv(envpath)
 env = os.environ
 
 DATABASE_URL = env.get('DATABASE_URL')
+if DATABASE_URL.startswith('postgres://'): DATABASE_URL = DATABASE_URL.replace('postgres://', 'postgresql://', 1)
 ES_USERID = env.get('ES_USERID')
 ES_PASSWORD = env.get('ES_PASSWORD')
 ES_HOST = env.get('ES_HOST')
