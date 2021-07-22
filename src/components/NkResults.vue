a96c443765f1dad7b12e87011c4a82ad2f43850b
unfif <age@sage.com>
2021-07-22 15:38:44 +0900

    場所形式での結果一覧表示部を、縦積み＆色分け表示することで可視性を向上 fix #193

--- src/components/NkResults.vue
+++ src/components/NkResults.vue
@@ -3,21 +3,26 @@
     <table class="placeinfo sticky table table-sm table-striped table-hover">
       <thead class="table-dark">
         <tr>
-          <th
-            v-for="column in results.schema.fields" :key="column.name"
+          <th v-for="column in results.schema.fields" :key="column.name"
             v-show="column.name !== '形式'"
-          >{{ column.name }}</th>
+          >
+            {{ column.name }}
+          </th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="courcedetails in results.data" :key="courcedetails.場所 + courcedetails.形式">
           <th class="table-secondary">
-            {{ courcedetails.場所 + courcedetails.形式 }}
+            <ul>
+              <li>{{ courcedetails.場所 }}</li>
+              <li>{{ courcedetails.形式 }}</li>
+            </ul>
           </th>
-          <td
-            v-for="(courcedetail, key) in courcedetails" :key="courcedetail"
+          <td v-for="(courcedetail, key) in courcedetails" :key="courcedetail"
             v-show="!(['場所', '形式'].includes(key))"
-          >{{ courcedetail }}</td>
+            v-html="displayResult(courcedetail, key)"
+          >
+          </td>
         </tr>
       </tbody>
     </table>
@@ -25,19 +30,61 @@
 </template>
 
 <script>
-import { reactive } from 'vue'
 export default {
   name: 'NkResults',
   props: {
     results: {
-      type: Object
+      type: Object,
+      default: ()=>{}
     }
   },
   setup(){
-    const data = reactive({
-      pass: null
-    })
-    return {data}
+    let wakuLists = [];
+
+    const displayResult = (results, key)=>{
+      if(Array.isArray(results)){
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        const liList = [];
+        let spanList = [];
+        if(key === '枠番') wakuLists = [];
+        let wakuList = [];
+        results.forEach((values, rowIndex)=>{
+          const pad = {word: ' ', length: 2};
+          if(key === '騎手'){pad.word = '　'; pad.length = 4;}
+          if(['枠番', '馬番', '人気', '騎手'].includes(key)){
+            spanList = [];
+            wakuList = [];
+            values.forEach((value, colIndex)=>{
+              span.innerText = value;
+              span.className = null;
+              if(key === '枠番'){
+                span.className = `postnum_${value}`;
+                wakuList.push(value);
+              }else if(key === '馬番'){
+                span.className = `postnum_${wakuLists[rowIndex][colIndex]}`;
+              }
+              span.classList.add(key);
+              if(key === '人気') span.classList.add(`rank_${value}`);
+              spanList.push(span.outerHTML);
+            })
+            li.innerHTML = spanList.join('');
+            if(key === '枠番') wakuLists.push(wakuList);
+          }else if(key === '距離'){
+            li.innerText = values;
+            li.className = values[0] <= 1600 ? 'short-distance' : 'long-distance';
+          }else{
+            li.innerText = values;
+          }
+          liList.push(li.outerHTML);
+        })
+        return '<ul>' + liList.join('') + '</ul>';
+      }
+    }
+
+    return {
+      displayResult
+    }
   }
 }
 </script>
@@ -57,6 +104,23 @@ table.sticky {
   th,
   td {
     white-space: nowrap;
+    font-family: Osaka-mono, "Osaka-等幅", "ＭＳ ゴシック", monospace;
   }
+}
+</style>
+<style>
+table.placeinfo ul {
+  list-style: none;
+  margin-top: 0;
+  margin-bottom: 0;
+  padding-left: 0;
+}
+table.placeinfo td span {
+  display: inline-block;
+  width: 16px;
+  text-align: center;
+}
+table.placeinfo td span.騎手 {
+    width: 3rem;
 }
 </style>
