91156d14e94ad5cac399db6e9b70f03f076e5950
unfif <age@sage.com>
2022-03-13 23:36:00 +0900

    レースの「条件」のスクレイピング結果が空文字やスペースの場合に、DBのraceconditionカラムへnullを登録するように修正 fix #267

---
+++ debug/check_blank_records_by_sqlalchemy.py
@@ -0,0 +1,45 @@
+# %%%
+from sqlalchemy import create_engine, text, inspect
+from sqlalchemy.future import select
+# from sqlalchemy.orm import Session
+from sqlalchemy.sql.sqltypes import TEXT
+import settings
+from pprint import pprint
+
+engine = create_engine(settings.DATABASE_URL, echo=False, future=True)
+inspector = inspect(engine)
+tables = inspector.get_table_names()
+
+# %%
+# for table in tables:
+#     with Session(engine, future=True) as session:
+#         columns = inspector.get_columns(table)
+#         for column in columns:
+#             if isinstance(column['type'], TEXT):
+#                 query = f"select * from {table} where {column['name']} = ' ' "
+#                 records = session.execute(text(query))
+#                 for record in records:
+#                     pprint(dict(record))
+
+with engine.connect() as conn:
+    for table in tables:
+        query = select('*').select_from(text(table))
+        columns = inspector.get_columns(table)
+        for column in columns:
+            if isinstance(column['type'], TEXT):
+                records = conn.execute(
+                    query.filter(text(f"{column['name']} = '' "))
+                )
+                for record in records:
+                    pprint(dict(record))
+
+# %%
+table = 'races'
+column = 'racecondition'
+columns = inspector.get_columns(table)
+for column in columns:
+    if column['name'] == 'racecondition':
+        pprint(column)
+
+print('done.')
+# %%
