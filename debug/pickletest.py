dd0addeea1e34728ebd520c493f983037577ffcf
unfif <age@sage.com>
2023-05-28 01:28:38 +0900

    elasticsearchが用意されていなくてもエラーで停止せずに実行可能に修正 fix #326

--- debug/pickletest.py
+++ debug/pickletest.py
@@ -7,9 +7,9 @@ for targetcol in ['単勝率', '連対率', '複勝率']:
     for place in set(jockeys.index.get_level_values(0)):
         tmprank = jockeys.loc[place, targetcol].rank(method='dense', ascending=False, na_option='bottom')
         tmprank.index = pd.MultiIndex.from_product([(place, ), tmprank.index])
-        jockeys.loc[place, targetcol + '順'] = tmprank
+        jockeys.loc[place, f'{targetcol}順'] = tmprank
 
-    jockeys[targetcol + '順'] = jockeys[targetcol + '順'].astype(int)
+    jockeys[f'{targetcol}順'] = jockeys[f'{targetcol}順'].astype(int)
 
 pass
 
--- debug/session_test.py
+++ debug/session_test.py
@@ -1,26 +1,26 @@
 from sqlalchemy import create_engine, select, update, delete, between, extract
 from sqlalchemy.orm import sessionmaker
-from nkdayscraper.models import Race, HorseResult, Racedates
+from nkdayscraper.models import Race, HorseResult, Racedate
 import datetime as dt
 from dotenv import dotenv_values
 from pprint import pprint
 
-config = dotenv_values('.env')
-DATABASE_URL = config['DATABASE_URL']
+env = dotenv_values('.env')
+DATABASE_URL = env['DATABASE_URL']
 engine = create_engine(DATABASE_URL, echo=True, future=True)
 # drop_table(engine, horseresults)
 # Base.metadata.create_all(engine)
 where = between(
-    Racedates.date,
+    Racedate.date,
     dt.date(2023, 3, 31),
     dt.date(2023, 4, 9)
 )
-where = Racedates.date == dt.date(2024, 1, 1)
-where = extract('year', Racedates.date) == 2023
-query = select(Racedates.date).where(where)
+where = Racedate.date == dt.date(2024, 1, 1)
+where = extract('year', Racedate.date) == 2023
+query = select(Racedate.date).where(where)
 rows = [
-    Racedates(date=dt.date(2024, 1, 1)),
-    Racedates(date=dt.date(2024, 1, 5)),
+    Racedate(date=dt.date(2024, 1, 1)),
+    Racedate(date=dt.date(2024, 1, 5)),
 ]
 query = select(Race).join(HorseResult.race)
 query = select(Race, HorseResult).outerjoin(HorseResult)
@@ -28,10 +28,10 @@ query = select(Race, HorseResult).outerjoin(HorseResult)
 Session = sessionmaker(bind=engine, future=True)
 with Session() as session:
     session.add_all(rows)
-    # record = session.query(Racedates).first()
+    # record = session.query(Racedate).first()
     records = session.execute(query).first()
     # records = session.scalars(query).all()
-    session.execute(delete(Racedates).where(where))
+    session.execute(delete(Racedate).where(where))
     # session.commit()
 
     # for record in records:
